ad2d301c300f7e63497d6d58adff2fa3a008ec6b
brianwyka <brian.wyka@gmail.com>
2020-11-25 15:21:59 -0500

    Attempted debian/rpm publish fixes for 409s

--- distributions/scripts/build-and-push-docker-builders-to-bintray.sh
+++ distributions/scripts/build-and-push-docker-builders-to-bintray.sh
@@ -8,8 +8,8 @@
 
 set -e
 
-DIR="$( cd "$( dirname "$( dirname "$( dirname "${BASH_SOURCE[0]}" )")")" && pwd )"
-DOCKER_BUILDERS_DIR="${DIR}../docker-builders"
+ROOT_DIR="$( cd "$( dirname "$( dirname "$( dirname "${BASH_SOURCE[0]}" )")")" && pwd )"
+DOCKER_BUILDERS_DIR="$ROOT_DIR/distributions/docker-builders"
 
 # Bintray Variables
 BINTRAY_API_URL="https://api.bintray.com"
@@ -18,15 +18,15 @@ BINTRAY_REPO="builders"
 REGISTRY="optum-docker-builders.bintray.io"
 
 # Native Image
-docker build -t $REGISTRY/nativeimage:graalvm-ce-20.3.0-java8 -f "$DOCKER_BUILDERS_DIR"/Dockerfile-nativeimage --build-arg FROM=oracle/graalvm-ce:20.3.0-java8 .
-docker build -t $REGISTRY/nativeimage:graalvm-ce-20.3.0-java11 -f "$DOCKER_BUILDERS_DIR"/Dockerfile-nativeimage --build-arg FROM=oracle/graalvm-ce:20.3.0-java11 .
+docker build -t $REGISTRY/nativeimage:graalvm-ce-20.3.0-java8 -f "$DOCKER_BUILDERS_DIR/Dockerfile-nativeimage" --build-arg FROM=oracle/graalvm-ce:20.3.0-java8 .
+docker build -t $REGISTRY/nativeimage:graalvm-ce-20.3.0-java11 -f "$DOCKER_BUILDERS_DIR/Dockerfile-nativeimage" --build-arg FROM=oracle/graalvm-ce:20.3.0-java11 .
 
 # RPM Build
-docker build -t $REGISTRY/rpmbuild:centos7 -f "$DOCKER_BUILDERS_DIR"/Dockerfile-rpmbuild --build-arg FROM=centos:7 .
-docker build -t $REGISTRY/rpmbuild:centos8 -f "$DOCKER_BUILDERS_DIR"/Dockerfile-rpmbuild --build-arg FROM=centos:8 .
-docker build -t $REGISTRY/rpmbuild:fedora32 -f "$DOCKER_BUILDERS_DIR"/Dockerfile-rpmbuild --build-arg FROM=fedora:32 .
-docker build -t $REGISTRY/rpmbuild:fedora33 -f "$DOCKER_BUILDERS_DIR"/Dockerfile-rpmbuild --build-arg FROM=fedora:33 .
-docker build -t $REGISTRY/rpmbuild:fedora34 -f "$DOCKER_BUILDERS_DIR"/Dockerfile-rpmbuild --build-arg FROM=fedora:34 .
+docker build -t $REGISTRY/rpmbuild:centos7 -f "$DOCKER_BUILDERS_DIR/Dockerfile-rpmbuild" --build-arg FROM=centos:7 .
+docker build -t $REGISTRY/rpmbuild:centos8 -f "$DOCKER_BUILDERS_DIR/Dockerfile-rpmbuild" --build-arg FROM=centos:8 .
+docker build -t $REGISTRY/rpmbuild:fedora32 -f "$DOCKER_BUILDERS_DIR/Dockerfile-rpmbuild" --build-arg FROM=fedora:32 .
+docker build -t $REGISTRY/rpmbuild:fedora33 -f "$DOCKER_BUILDERS_DIR/Dockerfile-rpmbuild" --build-arg FROM=fedora:33 .
+docker build -t $REGISTRY/rpmbuild:fedora34 -f "$DOCKER_BUILDERS_DIR/Dockerfile-rpmbuild" --build-arg FROM=fedora:34 .
 
 # Login to Registry
 echo "${BINTRAY_API_KEY}" | docker login --username "${BINTRAY_USERNAME}" --password-stdin $REGISTRY
