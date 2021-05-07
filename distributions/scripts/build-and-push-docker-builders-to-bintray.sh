6f5d1a540bb760e2596ad3fc9a8bff39d15e8808
brianwyka <brian.wyka@optum.com>
2021-05-07 11:15:19 -0700

    Update graal native image to 21.1.0

--- distributions/scripts/build-and-push-docker-builders-to-bintray.sh
+++ distributions/scripts/build-and-push-docker-builders-to-bintray.sh
@@ -18,8 +18,8 @@ BINTRAY_REPO="builders"
 REGISTRY="optum-docker-builders.bintray.io"
 
 # Native Image
-docker build -t $REGISTRY/nativeimage:graalvm-ce-21.0.0-java8 -f "$DOCKER_BUILDERS_DIR/Dockerfile-nativeimage" --build-arg FROM=ghcr.io/graalvm/graalvm-ce:java8-21.0.0 .
-docker build -t $REGISTRY/nativeimage:graalvm-ce-21.0.0-java11 -f "$DOCKER_BUILDERS_DIR/Dockerfile-nativeimage" --build-arg FROM=ghcr.io/graalvm/graalvm-ce:java11-21.0.0 .
+docker build -t $REGISTRY/nativeimage:graalvm-ce-21.1.0-java8 -f "$DOCKER_BUILDERS_DIR/Dockerfile-nativeimage" --build-arg FROM=ghcr.io/graalvm/graalvm-ce:java8-21.1.0 .
+docker build -t $REGISTRY/nativeimage:graalvm-ce-21.1.0-java11 -f "$DOCKER_BUILDERS_DIR/Dockerfile-nativeimage" --build-arg FROM=ghcr.io/graalvm/graalvm-ce:java11-21.1.0 .
 
 # RPM Build
 docker build -t $REGISTRY/rpmbuild:centos7 -f "$DOCKER_BUILDERS_DIR/Dockerfile-rpmbuild" --build-arg FROM=centos:7 .
@@ -32,8 +32,8 @@ docker build -t $REGISTRY/rpmbuild:fedora34 -f "$DOCKER_BUILDERS_DIR/Dockerfile-
 echo "${BINTRAY_API_KEY}" | docker login --username "${BINTRAY_USERNAME}" --password-stdin $REGISTRY
 
 # Push All Builders to Remote Registry
-docker push $REGISTRY/nativeimage:graalvm-ce-21.0.0-java8
-docker push $REGISTRY/nativeimage:graalvm-ce-21.0.0-java11
+docker push $REGISTRY/nativeimage:graalvm-ce-21.1.0-java8
+docker push $REGISTRY/nativeimage:graalvm-ce-21.1.0-java11
 docker push $REGISTRY/rpmbuild:centos7
 docker push $REGISTRY/rpmbuild:centos8
 docker push $REGISTRY/rpmbuild:fedora32
@@ -41,8 +41,8 @@ docker push $REGISTRY/rpmbuild:fedora33
 docker push $REGISTRY/rpmbuild:fedora34
 
 # Publish Images on Bintray
-curl -sfLS -X POST -u "${BINTRAY_USERNAME}:${BINTRAY_API_KEY}" $BINTRAY_API_URL/content/$BINTRAY_ORG/$BINTRAY_REPO/nativeimage/graalvm-ce-21.0.0-java8/publish
-curl -sfLS -X POST -u "${BINTRAY_USERNAME}:${BINTRAY_API_KEY}" $BINTRAY_API_URL/content/$BINTRAY_ORG/$BINTRAY_REPO/nativeimage/graalvm-ce-21.0.0-java11/publish
+curl -sfLS -X POST -u "${BINTRAY_USERNAME}:${BINTRAY_API_KEY}" $BINTRAY_API_URL/content/$BINTRAY_ORG/$BINTRAY_REPO/nativeimage/graalvm-ce-21.1.0-java8/publish
+curl -sfLS -X POST -u "${BINTRAY_USERNAME}:${BINTRAY_API_KEY}" $BINTRAY_API_URL/content/$BINTRAY_ORG/$BINTRAY_REPO/nativeimage/graalvm-ce-21.1.0-java11/publish
 curl -sfLS -X POST -u "${BINTRAY_USERNAME}:${BINTRAY_API_KEY}" $BINTRAY_API_URL/content/$BINTRAY_ORG/$BINTRAY_REPO/rpmbuild/centos8/publish
 curl -sfLS -X POST -u "${BINTRAY_USERNAME}:${BINTRAY_API_KEY}" $BINTRAY_API_URL/content/$BINTRAY_ORG/$BINTRAY_REPO/rpmbuild/fedora32/publish
 curl -sfLS -X POST -u "${BINTRAY_USERNAME}:${BINTRAY_API_KEY}" $BINTRAY_API_URL/content/$BINTRAY_ORG/$BINTRAY_REPO/rpmbuild/fedora33/publish
