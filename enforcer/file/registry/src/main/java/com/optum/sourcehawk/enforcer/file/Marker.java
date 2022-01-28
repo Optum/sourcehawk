0b1b97ac400dea93a00af96e97eb292345ed8121
Brian Wyka <brian.wyka@logicmonitor.com>
2022-01-27 21:43:40 -0500

    workflow-updates:  Make marker class public

--- enforcer/file/registry/src/main/java/com/optum/sourcehawk/enforcer/file/Marker.java
+++ enforcer/file/registry/src/main/java/com/optum/sourcehawk/enforcer/file/Marker.java
@@ -6,8 +6,10 @@ import com.optum.sourcehawk.enforcer.file.aot.SourcehawkFileEnforcerRegistry;
  * This is a marker interface to indicate that this package should generate a
  * registry of {@link com.optum.sourcehawk.enforcer.file.FileEnforcer}s
  *
+ * Internal use only
+ *
  * @author Brian Wyka
  */
 @SuppressWarnings("unused")
 @SourcehawkFileEnforcerRegistry
-interface Marker { }
+public interface Marker { }
