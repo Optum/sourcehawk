0414cee1ac512b650cfbfd3a71fd4ae300a6799b
Brian Wyka <brian.wyka@logicmonitor.com>
2022-01-27 20:01:52 -0500

    workflow-updates:  Use interface for aot annotation processing instead of package-info

--- enforcer/file/registry/src/main/java/com/optum/sourcehawk/enforcer/file/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This is a marker package-info to indicate that this package should generate a
- * registry of {@link com.optum.sourcehawk.enforcer.file.FileEnforcer}s
- *
- * @author Brian Wyka
- */
-@com.optum.sourcehawk.enforcer.file.aot.SourcehawkFileEnforcerRegistry
-//NOSONAR java:S4032
-package com.optum.sourcehawk.enforcer.file;
